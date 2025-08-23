8f574580523d1ef9625268973a24028e47cf9084
Vryon Antonio <vryonantonio@gmail.com>
2025-08-23 20:39:51 +0800

    cards shadow glow reduced

--- frontend/src/ui/Card.tsx
+++ frontend/src/ui/Card.tsx
@@ -6,7 +6,7 @@ interface CardProps {
 
 function Card({ children, onClick, additionalStyle = "" }: CardProps) {
   const style =
-    "rounded-2xl bg-neutral-50 px-4 py-3 drop-shadow-[0_3px_8px_rgba(0,0,0,0.24)] " +
+    "rounded-2xl bg-neutral-50 px-4 py-3 drop-shadow-[0_0_4px_rgba(0,0,0,0.24)] " +
     additionalStyle;
 
   return (
